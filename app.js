1f6e479884119029c30d0156d99c59137f624105
Katerberg <katerberg@fastmail.fm>
2016-11-14 21:39:57 -0600

    Makes world be generated

--- app.js
+++ app.js
@@ -1,128 +1,173 @@
-var express = require('express');
-var app = express();
-app.use(express.static('public'));
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var port = process.env.PORT || 3000;
+(function() {
+  'use strict';
+  const express = require('express');
+  const app = express();
+  app.use(express.static('public'));
+  const http = require('http').Server(app);
+  const io = require('socket.io')(http);
+  const port = process.env.PORT || 3000;
 
-// var lobbyUsers = {};
-// var users = {};
-// var activeGames = {};
+  const world = {
+    age: 0,
+    board: buildWorld(),
+    watchers: [],
+    checkForGrowth: function() {
+    }
+  };
 
-app.get('/', function(req, res) {
-  console.log('got called on base');
-  res.sendFile(__dirname + '/public/default.html');
-});
+  app.get('/', function(req, res) {
+    console.log('got called on base');
+    res.sendFile(__dirname + '/public/default.html');
+  });
 
-io.on('connection', function(socket) {
-    console.log('new connection ' + socket);
-    
-//     socket.on('login', function(userId) {
-//         console.log(userId + ' joining lobby');
-//         socket.userId = userId;  
-     
-//         if (!users[userId]) {    
-//             console.log('creating new user');
-//             users[userId] = {userId: socket.userId, games:{}};
-//         } else {
-//             console.log('user found!');
-//             Object.keys(users[userId].games).forEach(function(gameId) {
-//                 console.log('gameid - ' + gameId);
-//             });
-//         }
-        
-//         socket.emit('login', {users: Object.keys(lobbyUsers), 
-//                               games: Object.keys(users[userId].games)});
-//         lobbyUsers[userId] = socket;
-        
-//         socket.broadcast.emit('joinlobby', socket.userId);
-//     });
-    
-//     socket.on('invite', function(opponentId) {
-//         console.log('got an invite from: ' + socket.userId + ' --> ' + opponentId);
-        
-//         socket.broadcast.emit('leavelobby', socket.userId);
-//         socket.broadcast.emit('leavelobby', opponentId);
-      
+  app.get('/age', function(req, res) {
+    console.log('requesting age');
+    res.send(world.age);
+  });
+
+  app.get('/world', function(req, res) {
+    console.log('requesting world');
+    res.send(world.board);
+  });
+
+  function tick(socket) {
+    world.age++;
+    console.log('tick: ' + world.age);
+    world.watchers.forEach(function(watcher) {
+      watcher.emit('tick', world.age);
+    });
+    const worldDelta = world.checkForGrowth();
+    setTimeout(tick, 1000);
+  }
+
+  function kickoffTicker(socket) {
+    world.watchers.push(socket);
+    if (!world.age) {
+      tick();
+    }
+  }
+
+  function randomIntFromInterval(min,max)
+  {
+    return Math.floor(Math.random()*(max-min+1)+min);
+  }
+  
+  function buildWorld() {
+    const board = [];
+    for (let i = 0; i < 10; i++) {
+      board.push([]);
+      for (let j = 0; j < 10; j++) {
+        board[i].push({grass: 0});
+      }
+    }
+    const startX = randomIntFromInterval(0, 9),
+      startY = randomIntFromInterval(0, 9);
+    board[startX][startY].grass = 1;
+    return board;
+  }
+
+  io.on('connection', function(socket) {
+      console.log('new connection ' + socket.id);
+      kickoffTicker(socket);
+      buildWorld();
+
+  //     socket.on('login', function(userId) {
+  //         console.log(userId + ' joining lobby');
+  //         socket.userId = userId;  
        
-//         var game = {
-//             id: Math.floor((Math.random() * 100) + 1),
-//             board: null, 
-//             users: {white: socket.userId, black: opponentId}
-//         };
+  //         if (!users[userId]) {    
+  //             console.log('creating new user');
+  //             users[userId] = {userId: socket.userId, games:{}};
+  //         } else {
+  //             console.log('user found!');
+  //             Object.keys(users[userId].games).forEach(function(gameId) {
+  //                 console.log('gameid - ' + gameId);
+  //             });
+  //         }
+          
+  //         socket.emit('login', {users: Object.keys(lobbyUsers), 
+  //                               games: Object.keys(users[userId].games)});
+  //         lobbyUsers[userId] = socket;
+          
+  //         socket.broadcast.emit('joinlobby', socket.userId);
+  //     });
+      
+  //     socket.on('invite', function(opponentId) {
+  //         console.log('got an invite from: ' + socket.userId + ' --> ' + opponentId);
+          
+  //         socket.broadcast.emit('leavelobby', socket.userId);
+  //         socket.broadcast.emit('leavelobby', opponentId);
         
-//         socket.gameId = game.id;
-//         activeGames[game.id] = game;
-        
-//         users[game.users.white].games[game.id] = game.id;
-//         users[game.users.black].games[game.id] = game.id;
-  
-//         console.log('starting game: ' + game.id);
-//         lobbyUsers[game.users.white].emit('joingame', {game: game, color: 'white'});
-//         lobbyUsers[game.users.black].emit('joingame', {game: game, color: 'black'});
-        
-//         delete lobbyUsers[game.users.white];
-//         delete lobbyUsers[game.users.black];   
-        
-//         socket.broadcast.emit('gameadd', {gameId: game.id, gameState:game});
-//     });
-    
-//      socket.on('resumegame', function(gameId) {
-//         console.log('ready to resume game: ' + gameId);
          
-//         socket.gameId = gameId;
-//         var game = activeGames[gameId];
-        
-//         users[game.users.white].games[game.id] = game.id;
-//         users[game.users.black].games[game.id] = game.id;
-  
-//         console.log('resuming game: ' + game.id);
-//         if (lobbyUsers[game.users.white]) {
-//             lobbyUsers[game.users.white].emit('joingame', {game: game, color: 'white'});
-//             delete lobbyUsers[game.users.white];
-//         }
-        
-//         if (lobbyUsers[game.users.black]) {
-//             lobbyUsers[game.users.black] && 
-//             lobbyUsers[game.users.black].emit('joingame', {game: game, color: 'black'});
-//             delete lobbyUsers[game.users.black];  
-//         }
-//     });
+  //         var game = {
+  //             id: Math.floor((Math.random() * 100) + 1),
+  //             board: null, 
+  //             users: {white: socket.userId, black: opponentId}
+  //         };
+          
+  //         socket.gameId = game.id;
+  //         activeGames[game.id] = game;
+          
+  //         users[game.users.white].games[game.id] = game.id;
+  //         users[game.users.black].games[game.id] = game.id;
     
-//     socket.on('move', function(msg) {
-//         socket.broadcast.emit('move', msg);
-//         activeGames[msg.gameId].board = msg.board;
-//         console.log(msg);
-//     });
-    
-//     socket.on('disconnect', function(msg) {
-        
-//       console.log(msg);
+  //         console.log('starting game: ' + game.id);
+  //         lobbyUsers[game.users.white].emit('joingame', {game: game, color: 'white'});
+  //         lobbyUsers[game.users.black].emit('joingame', {game: game, color: 'black'});
+          
+  //         delete lobbyUsers[game.users.white];
+  //         delete lobbyUsers[game.users.black];   
+          
+  //         socket.broadcast.emit('gameadd', {gameId: game.id, gameState:game});
+  //     });
       
-//       if (socket && socket.userId && socket.gameId) {
-//         console.log(socket.userId + ' disconnected');
-//         console.log(socket.gameId + ' disconnected');
-//       }
-      
-//       delete lobbyUsers[socket.userId];
-      
-//       socket.broadcast.emit('logout', {
-//         userId: socket.userId,
-//         gameId: socket.gameId
-//       });
-//     });
-    
-//     /////////////////////
-//     // Dashboard messages 
-//     /////////////////////
-    
-//     socket.on('dashboardlogin', function() {
-//         console.log('dashboard joined');
-//         socket.emit('dashboardlogin', {games: activeGames}); 
-//     });
+  //      socket.on('resumegame', function(gameId) {
+  //         console.log('ready to resume game: ' + gameId);
            
-});
+  //         socket.gameId = gameId;
+  //         var game = activeGames[gameId];
+          
+  //         users[game.users.white].games[game.id] = game.id;
+  //         users[game.users.black].games[game.id] = game.id;
+    
+  //         console.log('resuming game: ' + game.id);
+  //         if (lobbyUsers[game.users.white]) {
+  //             lobbyUsers[game.users.white].emit('joingame', {game: game, color: 'white'});
+  //             delete lobbyUsers[game.users.white];
+  //         }
+          
+  //         if (lobbyUsers[game.users.black]) {
+  //             lobbyUsers[game.users.black] && 
+  //             lobbyUsers[game.users.black].emit('joingame', {game: game, color: 'black'});
+  //             delete lobbyUsers[game.users.black];  
+  //         }
+  //     });
+      
+  //     socket.on('move', function(msg) {
+  //         socket.broadcast.emit('move', msg);
+  //         activeGames[msg.gameId].board = msg.board;
+  //         console.log(msg);
+  //     });
+      
+  //     socket.on('disconnect', function(msg) {
+          
+  //       console.log(msg);
+        
+  //       if (socket && socket.userId && socket.gameId) {
+  //         console.log(socket.userId + ' disconnected');
+  //         console.log(socket.gameId + ' disconnected');
+  //       }
+        
+  //       delete lobbyUsers[socket.userId];
+        
+  //       socket.broadcast.emit('logout', {
+  //         userId: socket.userId,
+  //         gameId: socket.gameId
+  //       });
+  //     });
+  });
 
-http.listen(port, function() {
-    console.log('listening on *: ' + port);
-});
+  http.listen(port, function() {
+      console.log('listening on *: ' + port);
+  });
+})();
--- public/app.js
+++ public/app.js
@@ -2,4 +2,30 @@
   'use strict';
 
   var socket = io();
+
+  console.log('setting up listener ' + socket.id);
+  socket.on('tick', function(msg) {
+    console.log('ticked: ' + msg);
+    $('#tick').html(msg);
+  });
+
+  function buildRow(row) {
+    var domRow = '<div class="row">';
+    row.forEach(function(cell) {
+      domRow += '<div class="cell';
+      if (cell.grass) {
+        domRow += ' green';
+      }
+      domRow += '"></div>';
+    });
+    domRow += '</div>';
+    return domRow;
+  }
+
+  $.ajax('/world').then(function(response) {
+    var $board = $('#board');
+    response.forEach(function addRow(row) {
+      $board.append(buildRow(row));
+    });
+  });
 })();
