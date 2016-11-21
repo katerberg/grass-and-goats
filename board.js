ded67057400871013f4b835102aaff514415a0cd
Katerberg <katerberg@fastmail.fm>
2016-11-20 22:16:29 -0600

    Pushes more logic into grass

--- board.js
+++ board.js
@@ -22,51 +22,34 @@ class Board {
     }
     const startX = randomIntFromInterval(0, boardSize - 1),
       startY = randomIntFromInterval(0, boardSize - 1);
-    this.cells[startX][startY].grass = new Grass(1);
+    this.cells[startX][startY].grass.state = 1;
   }
 
   growGrass() {
     const result = [];
     this.cells.forEach(row => row.forEach(cell => {
-      cell.neighbors = this.numberOfGrassNeighbors(cell.x, cell.y);
-      if (cell.grass.state < 0.95) {
-        cell.push = {};
-        if (Math.random() < cell.neighbors * 0.1) {
-          cell.push.spread = true;
-        }
-        if (cell.grass.state !== 0) {
-          cell.push.grow = true;
-        }
-        if (Object.keys(cell.push).length) {
-          result.push(cell);
-        }
+      const neighbors = this.getNeighbors(cell.x, cell.y);
+      const growth = cell.grass.grow(neighbors);
+      if (growth) {
+        result.push(cell);
       }
     }));
-    result.forEach(cell => {
-      if (cell.push.spread) {
-        cell.grass.state += 0.1;
-      }
-      if (cell.push.grow) {
-        cell.grass.state += (0.05 - 0.01 * cell.neighbors);
-      }
-      this.cells[cell.x][cell.y].grass = cell.grass;
-    });
     return result;
   }
 
-  numberOfGrassNeighbors(cellX, cellY) {
-    let result = 0;
+  getNeighbors(cellX, cellY) {
+    const result = [];
     if (cellX !== 0) {
-      result += this.cells[cellX - 1][cellY].grass.state;
+      result.push(this.cells[cellX - 1][cellY].grass);
     }
     if (cellX !== this.cells.length - 1) {
-      result += this.cells[cellX + 1][cellY].grass.state;
+      result.push(this.cells[cellX + 1][cellY].grass);
     }
     if (cellY !== 0) {
-      result += this.cells[cellX][cellY - 1].grass.state;
+      result.push(this.cells[cellX][cellY - 1].grass);
     }
     if (cellY !== this.cells[cellX].length - 1) {
-      result += this.cells[cellX][cellY + 1].grass.state;
+      result.push(this.cells[cellX][cellY + 1].grass);
     }
     return result;
   }
--- grass.js
+++ grass.js
@@ -3,6 +3,30 @@
 class Grass {
   constructor (state) {
     this.state = state ? state : 0;
+    this.neighbors = [];
+  }
+
+  grassSurrounding(neighbors) {
+    return neighbors.reduce((sum, neighbor) => {
+      return sum + neighbor.state;
+    }, 0);
+  }
+
+  becomesColonized(neighbors) {
+    return Math.random() < 0.1 * this.grassSurrounding(neighbors);
+  }
+
+  grow(neighbors) {
+    let startingState = this.state;
+    if (this.state < 0.95) {
+      if (this.state !== 0) {
+        this.state += (0.05 - 0.01 * this.grassSurrounding(neighbors));
+      }
+      if (this.becomesColonized(neighbors)) {
+        this.state += 0.1;
+      }
+    }
+    return startingState !== this.state;
   }
 }
 
